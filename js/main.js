8ff5edb5ffd6795459ca129774ffb4a698793ffe
Pritam Umaiya <pritamumaiya@gmail.com>
2025-01-01 15:16:07 +0000

    removed diary

--- js/main.js
+++ js/main.js
@@ -4,20 +4,3 @@ document.addEventListener('DOMContentLoaded', () => {
     const initialTheme = (currentHour >= 6 && currentHour < 18) ? 'light' : 'dark';
     document.body.setAttribute('data-bs-theme', initialTheme);
 });
-
-function submitFormAndDownload() {
-    // Create a FormData object from the form
-    const form = document.getElementById('diary-download-form');
-    const formData = new FormData(form);
-    
-    // Send form data asynchronously via fetch
-    fetch("/", {
-      method: "POST",
-      body: formData,
-    })
-    .then(() => {
-      // After successful form submission, trigger the file download
-      window.location.href = 'Dear_Umaiya.pdf';
-    })
-    .catch((error) => console.error('Error:', error));
-}
